e8bc5a3c3e5bd66d695c5c0add548267a1101c3f
diiyavol <nikitanesterenko88@gmail.com>
2026-08-08 10:41:54 +0300

    Solution 2

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,20 +1,25 @@
 'use strict';
 
 document.addEventListener('click', (e) => {
-  const posX = event.offsetX;
-  const posY = event.offsetY;
   const elemWall = e.target.closest('.wall');
+  const elemSpider = document.querySelector('.spider');
 
   if (!elemWall) {
     return;
   }
 
-  const elemSpider = document.querySelector('.spider');
-  let newTop = Math.max(posY - elemSpider.clientHeight / 2, 0);
-  let newLeft = Math.max(posX - elemSpider.clientWidth / 2, 0);
+  const rectWall = elemWall.getBoundingClientRect();
+  const rectSpider = elemSpider.getBoundingClientRect();
 
-  newTop = Math.min(newTop, elemWall.clientHeight - elemSpider.clientHeight);
-  newLeft = Math.min(newLeft, elemWall.clientWidth - elemSpider.clientWidth);
+  const posX = e.clientX - rectWall.left;
+  const posY = e.clientY - rectWall.top;
+
+  let newTop = Math.max(posY - rectSpider.height / 2, 0);
+  let newLeft = Math.max(posX - rectSpider.width / 2, 0);
+
+  newTop = Math.min(newTop, rectWall.height - rectSpider.height);
+  newLeft = Math.min(newLeft, rectWall.width - rectSpider.width);
   elemSpider.style.left = `${newLeft}px`;
   elemSpider.style.top = `${newTop}px`;
+  elemSpider.style.position = 'absolute';
 });
